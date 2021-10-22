3d280af722ed3adb1017db157d424242256a9e0c
AdvaitaAmit <85395773+AdvaitaAmit@users.noreply.github.com>
2021-10-22 17:47:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -43,7 +43,7 @@ last_position_of_y = e.touches[0].clientY-canvas.offsetTop
         ctx.moveTo(last_position_of_x, last_position_of_y);
 
         console.log("Current position of x and y coordinates = ");
-        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
+        console.log("x  = " + current_position_of_x + "y = " + current_position_of_y);
         ctx.lineTo(current_position_of_x, current_position_of_y);
         ctx.stroke();
         
